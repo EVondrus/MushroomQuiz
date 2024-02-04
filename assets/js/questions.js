bddf2d0f45c327a6fdee0c3fb1d71cd097a37797
Ellenor Vondrus <150965114+EVondrus@users.noreply.github.com>
2024-02-04 17:33:58 +0000

    Create and add  questions into questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,97 @@
+// Creating an array passing the questions, options and answers
+let questions = [
+  {
+    question: "What is the main purpose of a mushroom cap?"
+    answers: [
+      { text: "Reproduction", correct: false },
+      { text: "Protection", correct: true },
+      { text: "Collecting sunlight", correct: false },
+      { text: "Attracting insects", correct: false },
+       
+    ]
+
+  },
+  {
+    question: "Which mushroom is commonly used in cooking?"
+    answers: [
+      { text: "Fly Agaric", correct: false },
+      { text: "Autumn Skullcap", correct: false },
+      { text: "Button Mushroom", correct: true },
+      { text: "Inky Cap", correct: false },
+       
+    ]
+  },
+  {
+    question: "Where are spores produced in a mushroom?"
+    answers: [
+      { text: "Stipe", correct: false },
+      { text: "Gills", correct: true },
+      { text: "Cap", correct: false },
+      { text: "Annulus", correct: false },
+    ]
+  },
+  {
+    question: "Which mushroom is known for it's glowing in the dark properties?"
+    answers: [
+      { text: "Morel", correct: false },
+      { text: "Chanterelle", correct: false },
+      { text: "Shiitake", correct: false },
+      { text: "Jack O'Lantern", correct: true },
+    ]
+  },
+  {
+    question: "Which toxic mushroom has a bright red cap with white dots?"
+    answers: [
+      { text: "Fly Agaric", correct: true },
+      { text: "Death Cap", correct: false },
+      { text: "Sickener", correct: false },
+      { text: "False Morel", correct: false },
+    ]
+  },
+  {
+    question: "What is the common name for Pleurotus ostreatus?"
+    answers: [
+      { text: "Shiitake", correct: false },
+      { text: "Oyster Mushroom", correct: true },
+      { text: "Enoki", correct: false },
+      { text: "Morel", correct: false },
+    ]
+  },
+  {
+    question: "What is the primary role of the mycelium in mushrooms?"
+    answers: [
+      { text: "Producing spores", correct: false },
+      { text: "Conducting photosynthesis", correct: false },
+      { text: "Exchanging gases", correct: false },
+      { text: "Absorbing nutrients", correct: true },
+    ]
+  },
+  {
+    question: "Which mushroom is often used as a meat substitute in vegetarian dishes?"
+    answers: [
+      { text: "Portabello", correct: true },
+      { text: "Enoki", correct: false },
+      { text: "Oyster", correct: false },
+      { text: "Maitake", correct: false },
+    ]
+  },
+  {
+    question: "Which mushroom is known for its medicinal benefits?"
+    answers: [
+      { text: "Morel", correct: false },
+      { text: "Chanterelle", correct: false },
+      { text: "Cremin", correct: false },
+      { text: "Maitake", correct: true },
+    ]
+  },
+  {
+    question: "What is the most commonly cultivated mushroom worldwide?"
+    answers: [
+      { text: "Lions Mane", correct: false },
+      { text: "Chanterelle", correct: false },
+      { text: "White button", correct: true },
+      { text: "Oyster", correct: false },
+    ]
+  }
+
+];
