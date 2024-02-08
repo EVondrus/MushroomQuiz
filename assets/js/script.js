2172e546cd90fbaf728741c399b96d4ad3c31e97
Ellenor Vondrus <150965114+EVondrus@users.noreply.github.com>
2024-02-08 15:26:43 +0000

    Implement startTimer function with reset, display update, and interval setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -300,12 +300,6 @@ function startTimer() {
 }
 
 
-
-//Timer function
-//if timer is less than 0
-//clear interval, clear counter
-
-
 //startTimerLine function- Animation
 // Set an interval to repeatedly call the 'timer' function
 // define the timer function
