8cb15259c5f76ae21a1ae14fd36ebcc1c3e4ee1f
Ellenor Vondrus <150965114+EVondrus@users.noreply.github.com>
2024-02-10 17:01:30 +0000

    Updated comments nd function documentation for clarity and consis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,77 @@
-//Select all elements required
+//Select the elements required
 const startBtn = document.querySelector(".start-btn button");
 const infoBox = document.querySelector(".info-box");
 const exitBtn = infoBox.querySelector(".buttons .quit-btn");
 const continueBtn = infoBox.querySelector(".buttons .restart-btn");
 const quizBox = document.querySelector(".quiz-box");
+const timeCount = document.querySelector(".timer .timer-sec");
+const timeLineElement = document.querySelector("header .time-line");
 const questionElement = document.getElementById("question");
+const question = document.getElementById("question");
 const answerOptions = document.querySelector(".answer-options");
 const resultBox = document.querySelector(".result-box");
-const timeLineElement = document.querySelector("header .time-line");
-const timeCount = document.querySelector(".timer .timer-sec");
+const restartBtn = resultBox.querySelector(".buttons .restart-btn");
 const quitBtn = resultBox.querySelector(".buttons .quit-btn");
 
-// if startQuiz (startBtn) clicked
-//show info box, add activeInfo class
-startBtn.onclick = ()=>{
-  questions = shuffle(questions); //Shuffle questions
-  infoBox.classList.add("activeInfo"); 
-};
+// START BUTTON
 
-//Info Box
+// Event handler for the start button click
+startBtn.onclick = () => {
+  questions = shuffle(questions); // Shuffle the questions
+  infoBox.classList.add("activeInfo"); // Show the information box
+}
 
-// if exitQuiz (exitBtn) clicked
-//hide info box, remove activeInfo class
-exitBtn.onclick = ()=>{
+//INFO BOX
+
+// Event handler for the exit button click
+exitBtn.onclick = () => {
   infoBox.classList.remove("activeInfo");
-};
+}
 
+// Event handler for the continue button click
+continueBtn.onclick = () => {
+  infoBox.classList.remove("activeInfo"); // Hide Info box
+  quizBox.classList.add("activeQuiz"); // Show Quiz box
+  showQuestions(); //Dislpay Questions
+  startTimer(); // Start Timer countdown
+  startTimerLine(); // Start Timer Line
+}
 
-// if continueBtn clicked
-continueBtn.onclick = ()=>{
-//hide info box, show quiz box
-  infoBox.classList.remove("activeInfo"); 
-  quizBox.classList.add("activeQuiz"); 
-//call function to run the questions and answer
-  showQuestions(); 
-//give 1 parameter to current question
-//call startTimer function
-startTimer();
-//call startTimerLine function
-startTimerLine();
-};
+// Variables to store various values and states during the quiz
+let time = 15; // Initial time for each question
+let timerInterval; // Interval for the timer countdown
+let timerLine; // Interval for the timer line animation
+let currentQuestionIndex = 0; // Index of the current question
+let correctScore = 0; // Number of correctly answered questions
+let incorrectScore = 0; // Number of incorrectly answered questions
 
+//QUIZ BOX
 
-
-/*Create variables to store the questions count, question number, counter, counterLine, width value and time value*/
-let time = 15;
-let timerInterval;
-let timerLine;
-let currentQuestionIndex = 0;
-let correctScore = 0;
-let incorrectScore = 0;
-
-
-//Quiz Box
-//get elements
-const question = document.getElementById("question");
-const nextBtn = document.querySelector("footer .next-btn");
-
-// Function to shuflle the questions
-// Credit: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+/**
+ * Shuffles the order of elements in an array 
+ * Using the Fisher-Yates shuffle algorithm. See the README.md for acknowledgement.
+ */
 function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
+  let currentIndex = array.length,
+    randomIndex;
 
   // While there remain elements to shuffle.
   while (currentIndex > 0) {
-
     // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
     // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
 
   return array;
 }
 
-
 // Create an array passing the questions, answer options and set the correct answer
-// Get from question.js
 let questions = [
   {
     question: "What is the main purpose of a mushroom cap?",
@@ -88,8 +80,7 @@ let questions = [
       { text: "Protection", correct: true },
       { text: "Collecting sunlight", correct: false },
       { text: "Attracting insects", correct: false },
-       
-    ]
+    ],
   },
   {
     question: "Which mushroom is commonly used in cooking?",
@@ -98,8 +89,7 @@ let questions = [
       { text: "Autumn Skullcap", correct: false },
       { text: "Button Mushroom", correct: true },
       { text: "Inky Cap", correct: false },
-       
-    ]
+    ],
   },
   {
     question: "Where are spores produced in a mushroom?",
@@ -108,16 +98,17 @@ let questions = [
       { text: "Gills", correct: true },
       { text: "Cap", correct: false },
       { text: "Annulus", correct: false },
-    ]
+    ],
   },
   {
-    question: "Which mushroom is known for it's glowing in the dark properties?",
+    question:
+      "Which mushroom is known for it's glowing in the dark properties?",
     answers: [
       { text: "Morel", correct: false },
       { text: "Chanterelle", correct: false },
       { text: "Shiitake", correct: false },
       { text: "Jack O'Lantern", correct: true },
-    ]
+    ],
   },
   {
     question: "Which toxic mushroom has a bright red cap with white dots?",
@@ -126,7 +117,7 @@ let questions = [
       { text: "Death Cap", correct: false },
       { text: "Sickener", correct: false },
       { text: "False Morel", correct: false },
-    ]
+    ],
   },
   {
     question: "What is the common name for Pleurotus ostreatus?",
@@ -135,7 +126,7 @@ let questions = [
       { text: "Oyster Mushroom", correct: true },
       { text: "Enoki", correct: false },
       { text: "Morel", correct: false },
-    ]
+    ],
   },
   {
     question: "What is the primary role of the mycelium in mushrooms?",
@@ -144,16 +135,17 @@ let questions = [
       { text: "Conducting photosynthesis", correct: false },
       { text: "Exchanging gases", correct: false },
       { text: "Absorbing nutrients", correct: true },
-    ]
+    ],
   },
   {
-    question: "Which mushroom is often used as a meat substitute in vegetarian dishes?",
+    question:
+      "Which mushroom is often used as a meat substitute in vegetarian dishes?",
     answers: [
       { text: "Portabello", correct: true },
       { text: "Enoki", correct: false },
       { text: "Oyster", correct: false },
       { text: "Maitake", correct: false },
-    ]
+    ],
   },
   {
     question: "Which mushroom is known for its medicinal benefits?",
@@ -162,7 +154,7 @@ let questions = [
       { text: "Chanterelle", correct: false },
       { text: "Cremin", correct: false },
       { text: "Maitake", correct: true },
-    ]
+    ],
   },
   {
     question: "What is the most commonly cultivated mushroom worldwide?",
@@ -171,188 +163,204 @@ let questions = [
       { text: "Chanterelle", correct: false },
       { text: "White button", correct: true },
       { text: "Oyster", correct: false },
-    ]
-  }
-
+    ],
+  },
 ];
 
-
-/* Show question function */
+/**
+ * Function to display the current question and its answer options to the user.
+ * It clears the previous answer options, then retrieves the current question from the 'questions' array.
+ * The question number is updated and displayed along with the question text.
+ * Answer options are shuffled for each question and displayed as buttons.
+ * Event listeners are set for each answer button to handle user selection.
+ */
 function showQuestions() {
   resetState(); // Remove the previous answer options
-let currentQuestion = questions[currentQuestionIndex]; // Get the current question from the array
-let questionNumber = currentQuestionIndex + 1; // Add 1 to index to display the correct question number
-// Display the question to user
-question.innerHTML = questionNumber + ". " + currentQuestion.question;
-console.log(answerOptions);
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNumber = currentQuestionIndex + 1;
 
-// Update the current question number in the span
-document.getElementById('current-question').textContent = questionNumber;
-
-// Display the answer options to the user
-currentQuestion.answers = shuffle(currentQuestion.answers);
-currentQuestion.answers.forEach(answer => {
-  let button = document.createElement("button");
-  button.innerText = answer.text;
-  button.classList.add("answer-btn");
-  answerOptions.appendChild(button);
-
-  // Set the data attribute for the correct answer
-  if (answer.correct) {
-    button.dataset.correct = answer.correct; 
-  }
-
-  // Set event listener for answer buttons with select answer function
-  button.addEventListener("click", selectAnswer);
-});
+  question.innerHTML = questionNumber + ". " + currentQuestion.question;
+  document.getElementById("current-question").textContent = questionNumber; // Update the current question number in the span
+  // Shuffle the answer options to randomize their order
+  currentQuestion.answers = shuffle(currentQuestion.answers);
+  // Iterate through each answer option and create a button for it
+  currentQuestion.answers.forEach((answer) => {
+    let button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("answer-btn");
+    answerOptions.appendChild(button);
+    // Set a data attribute for the correct answer
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
 }
 
-
-/* Function to clear the answer options */
+/**
+ * Function to remove the previously displayed answer options from the DOM.
+ * It removes all child elements of the 'answerOptions' element until none remain.
+ */
 function resetState() {
   while (answerOptions.firstChild) {
     answerOptions.removeChild(answerOptions.firstChild);
   }
 }
 
-/* Function for selected answers
-Check if correct,
-Display incorrect and/or correct answer
-Increase the score of correct or incorrect
-Disable answer buttons,
-Show next question after 1,5sec */
-
-/** Selected answer */
+/**
+ * Handles the user's selection of an answer.
+ * @param {Event} e - The event object triggered by the button click.
+ */
 function selectAnswer(e) {
-  const selectedBtn = e.target;
-  const isCorrect = selectedBtn.dataset.correct === "true";
-  
+  let selectedBtn = e.target;
+  let isCorrect = selectedBtn.dataset.correct === "true";
+
   clearInterval(timerInterval); //Stop the timer when any answer buttons are clicked
   clearInterval(timerLine); //Stop the timer line when any answer buttons are clicked
 
-  if (isCorrect){
+  // Add appropriate classes and update scores based on correctness of the selected answer
+  if (isCorrect) {
     selectedBtn.classList.add("correct");
     correctScore++;
-    document.getElementById('correct-score').textContent = correctScore;
+    document.getElementById("correct-score").textContent = correctScore;
   } else {
     selectedBtn.classList.add("incorrect");
     incorrectScore++;
-    document.getElementById('incorrect-score').textContent = incorrectScore;
+    document.getElementById("incorrect-score").textContent = incorrectScore;
   }
-  Array.from(answerOptions.children).forEach(button => {
-    if (button.dataset.correct === "true"){
+  // Disable all answer buttons and highlight the correct one
+  Array.from(answerOptions.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
       button.classList.add("correct");
     }
     button.disabled = true;
   });
-  setTimeout(showNextQuestion, 1500);
+  setTimeout(showNextQuestion, 1500); //Show next question after 1,5sec
 }
 
-
-/** Display Next Question */
+/**
+ * Display the next question or show the result if all questions have been answered.
+ */
 function showNextQuestion() {
   if (currentQuestionIndex < questions.length - 1) {
-    currentQuestionIndex++; //increment the current question number in header
+    currentQuestionIndex++; //increment the current question number
     showQuestions();
     startTimer();
     startTimerLine();
   } else {
-    // Otherwise, if there are no more questions remaining, show the result
+    // If there are no more questions remaining, show the Result box
     showResult();
   }
 }
 
+// RESULT BOX
 
-//Result Box 
-
-//showResult function
-//hide info box, remove activeInfo class
-//hide quiz box, remove activeQuiz class
-//show result box, add activeResult class
-//pass the users score, total question number and message
-function showResult(){
+/**
+ * showResult function:
+ * Removes the activeInfo class from infoBox, removes the activeQuiz class from quizBox,
+ * and adds the activeResult class to resultBox to display the result. Determines the score message
+ * based on the number of correct answers and updates the final score element accordingly.
+ */
+function showResult() {
   infoBox.classList.remove("activeInfo");
   quizBox.classList.remove("activeQuiz");
   resultBox.classList.add("activeResult");
-  let finalScoreElement = resultBox.querySelector(".final-score"); 
 
+  let finalScoreElement = resultBox.querySelector(".final-score");
   let scoreMessage;
-  if (correctScore <= 4){
-    scoreMessage = '<span>You got <p>'+ correctScore +'</p> out of <p>'+ questions.length +'</p> Better luck next time!</span>';
-  }
-  else if (correctScore >= 5 && correctScore <= 7){
-    scoreMessage = '<span>You got <p>'+ correctScore +'</p> out of <p>'+ questions.length +'</p> Good job! Just a little more now!</span>';
-  }
-  else if (correctScore >= 8 && correctScore <= 10) {
-    scoreMessage = '<span>You got <p>'+ correctScore +'</p> out of <p>'+ questions.length +'</p> AWESOME! You know your mushrooms!</span>';
+  // Determine the score message based on the number of correct answers
+  if (correctScore <= 4) {
+    scoreMessage =
+      "<span>You got <p>" +
+      correctScore +
+      "</p> out of <p>" +
+      questions.length +
+      "</p> Better luck next time!</span>";
+  } else if (correctScore >= 5 && correctScore <= 7) {
+    scoreMessage =
+      "<span>You got <p>" +
+      correctScore +
+      "</p> out of <p>" +
+      questions.length +
+      "</p> Good job! Just a little more now!</span>";
+  } else if (correctScore >= 8 && correctScore <= 10) {
+    scoreMessage =
+      "<span>You got <p>" +
+      correctScore +
+      "</p> out of <p>" +
+      questions.length +
+      "</p> AWESOME! You know your mushrooms!</span>";
   }
   finalScoreElement.innerHTML = scoreMessage;
-
 }
 
-
-
-
-// if quitBtn clicked
+// If the quit button is clicked, reload the current window to restart the quiz
 quitBtn.onclick = () => {
-  window.location.reload(); // Reload the current window
+  window.location.reload();
 }
 
-//TODO: If restartBtn is clicked
-// showQuestion function
+//If restartBtn is clicked
+restartBtn.onclick = () => {
+  // Remove the result box and show the quiz box again
+  resultBox.classList.remove("activeResult");
+  quizBox.classList.add("activeQuiz");
+  // Reset question index and scores
+  currentQuestionIndex = 0;
+  correctScore = 0;
+  incorrectScore = 0;
+  // Display the first question and start the timer and timer line
+  showQuestions();
+  startTimer();
+  startTimerLine();
+}
 
-
-
-/* Start timer function */
+/**
+ *Start timer function:
+ * Resets the timer to 15 seconds, updates the timer display, clears any existing interval to prevent overlapping timers,
+ * and starts a new interval that decrements the timer every second. If the time reaches 0, increments the incorrect score
+ * if no answer is selected, updates the display, and either moves to the next question or shows the result if no more questions remain.
+ */
 function startTimer() {
-  time = 15; // Reset the time to 15 seconds
+  time = 15;
   timeCount.textContent = time; // Update the timer display initially
   clearInterval(timerInterval); // Clear any existing interval, prevent overlapping timers
-  // Start the interval
-  timerInterval = setInterval(() => { 
+  timerInterval = setInterval(() => {
     if (time <= 0) {
       clearInterval(timerInterval); // Stop the timer if time reaches 0
       incorrectScore = incorrectScore + 1; // Incrementing incorrect score if no answer selected
-      document.getElementById('incorrect-score').textContent = incorrectScore;
+      document.getElementById("incorrect-score").textContent = incorrectScore;
 
       if (currentQuestionIndex < questions.length - 1) {
         currentQuestionIndex++;
         showQuestions();
-        startTimer(); // Restart the timer for the next question
+        startTimer();
       } else {
-        showResult(); // Show the result if no more questions remaining
+        showResult();
       }
     } else {
       time--; // Decrement the time if it's greater than 0
-      timeCount.textContent = time;// Update the timer display
+      timeCount.textContent = time; // Update the timer display
     }
   }, 1000); // Repeat every 1 second (1000 milliseconds)
 }
 
-
-
-//TODO: startTimerLine function- Animation
-// Set an interval to repeatedly call the 'timer' function
-// define the timer function
-// increment the 'time' variable by 1 on each interval
-// update the width of the HTML element. The width is increased by the value of 'time' "%"
-// if the time value exceeds 100%
-//clear counterLine
-
-
-// Start timer line function
+/**
+ * Start timer line function:
+ * Initializes the time for the timer line, clears any existing timer line interval, and sets a new interval
+ * for updating the width of the timer line element. The interval adjusts the time and updates the width
+ * until the time reaches 100%, at which point the timer is stopped.
+ */
 function startTimerLine() {
-  let time = 0; // Initialize the time for the time line
-  clearInterval(timerLine); // Clear any existing timer line interval
-  timerLine = setInterval(timer, 150); // Adjust the interval as needed
-
+  let time = 0;
+  clearInterval(timerLine);
+  timerLine = setInterval(timer, 150);
+  //Increments the time by 1 on each interval, updates the width of the time line element,  
   function timer() {
-    time += 1; // Increment the time by 1
-    timeLineElement.style.width = time + "%"; // Update the width of the time line element
+    time += 1;
+    timeLineElement.style.width = time + "%";
 
     if (time >= 100) {
-      clearInterval(timerLine); // Stop the timer when it reaches 100%
+      clearInterval(timerLine);
     }
   }
 }
-
